761b4dc9330dcb4ea0448552629b7ff2becc7574
Sagnik Sarkar <sagniksnow@gmail.com>
2025-10-28 12:31:04 +0530

    Change logo emoji from DNA to bubble

--- streamlit_app.py
+++ streamlit_app.py
@@ -699,7 +699,7 @@ def main():
     # Fixed Header/Logo Section
     st.markdown(
         f'<div style="display: flex; align-items: center; padding-top: 18px;">'
-        f'<h3 style="color: #007AFF; margin-right: 10px; margin-top: 0; font-weight: 700;">🧬</h3>'
+        f'<h3 style="color: #007AFF; margin-right: 10px; margin-top: 0; font-weight: 700;">🫧</h3>'
         f'<h3 style="margin: 0; font-weight: 500;">PFAS Toxicity Classifier</h3>'
         f'</div>', 
         unsafe_allow_html=True
